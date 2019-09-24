cda084f3385df179c65a94483712064287ea04ab
Larissa Reis <larissa.candeia@gmail.com>
2019-09-23 22:05:45 -0300

    .

--- js/ui.js
+++ js/ui.js
@@ -2,3 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
 });
+
+$('#textarea1').val('Novo Texto');
+  M.textareaAutoResize($('#textarea1'));
