949756b07e5a6011c8d135f000af1ef652849a02
Larissa Reis <larissa.candeia@gmail.com>
2019-09-30 20:34:42 -0300

    adicionando o codigo para fetch sw

--- sw.js
+++ sw.js
@@ -5,3 +5,8 @@ self.addEventListener('install', evt => {
 self.addEventListener('activate', evt => {
 	console.log('Service worker foi ativado.');
 });
+
+self.addEventListener('fetch', evt => {
+	console.log('Service worker capturou um evento do tipo fetch.');
+});
+
