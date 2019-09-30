a9362650e09d20fd90fa7a719e52b6ef20f7520f
Larissa Reis <larissa.candeia@gmail.com>
2019-09-30 20:05:48 -0300

    adicionando o codigo para activate no sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener('install', evt => {
 	console.log('Service worker foi instalado.');
 });
+
+self.addEventListener('activate', evt => {
+	console.log('Service worker foi ativado.');
+});
