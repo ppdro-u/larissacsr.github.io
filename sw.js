ed6d133fa4e19ef42bbde9d67df78953fafde608
Larissa Reis <larissa.candeia@gmail.com>
2019-10-10 00:37:15 -0300

    adicionando cache para alguns recursos no evento de instalacao do pwa

--- sw.js
+++ sw.js
@@ -1,5 +1,25 @@
+const staticCacheName = 'site-static';
+
+const recursos = [
+	'/',
+	'/index.html',
+	'/js/app.js',
+	'/js/ui.js',
+	'/js/materialize.min.js',
+	'/css/materialize.min.css',
+	'/img/2810845dae5561a91562998ba4cf8939.jpg',
+	'/img/Red-Velvet-Cake-8.jpg',
+	'/img/Cheesecake.jpg',
+];
+
 self.addEventListener('install', evt => {
-	console.log('Service worker foi instalado.');
+	// console.log('Service worker foi instalado.');
+	evt.waitUntil(
+		caches.open(staticCacheName).then(cache => {
+			console.log("Guardando dados em cache");
+			cache.addAll(recursos);
+		})
+	);
 });
 
 self.addEventListener('activate', evt => {
