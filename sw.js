fb97b9b4270be476b38555b8460ed0c51a3bd37a
Larissa Reis <larissa.candeia@gmail.com>
2019-10-16 17:26:36 -0300

    mudando o sw e adicionando pg de fallback

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-//1
-const staticCacheName = 'site-static';
-//1
-const recursos = [
+const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v1';
+//aqui ficam os recursos que você quer fazer cache
+const assets = [
 	'/',
 	'/index.html',
 	'/js/app.js',
@@ -11,31 +11,104 @@ const recursos = [
 	'/img/2810845dae5561a91562998ba4cf8939.jpg',
 	'/img/Red-Velvet-Cake-8.jpg',
 	'/img/Cheesecake.jpg',
+	'https://fonts.googleapis.com/icon?family=Material+Icons',
+	'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+	'/pages/fallback.html'
 ];
+
+// essa linha limita o tamanho do cache
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
+// evento de instalação do service worker
+self.addEventListener('install', evt => {
+  //console.log('service worker instalado');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  //console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      //console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          // check cached items size
+          limitCacheSize(dynamicCacheName, 15);
+          return fetchRes;
+        })
+      });
+    }).catch(() => {
+      if(evt.request.url.indexOf('.html') > -1){
+        return caches.match('/pages/fallback.html');
+      } 
+    })
+  );
+});
 //1
 self.addEventListener('install', evt => {
-	// console.log('Service worker foi instalado.');
-	evt.waitUntil(
-		caches.open(staticCacheName).then(cache => {
-			console.log("Guardando dados em cache");
-			cache.addAll(recursos);
-		})
-	);
+    // console.log('Service worker foi instalado.');
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
 
 });
 
 self.addEventListener('activate', evt => {
-	console.log('Service worker foi ativado.');
+    console.log('Service worker foi ativado.');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
 });
 
 //2
 self.addEventListener('fetch', evt => {
-	// console.log('Service worker capturou um evento do tipo fetch.');
-	evt.respondWith(
-		caches.match(evt.request).then(cacheRes =>{
-			return cacheRes || fetch(evt.request);
-		})
-	)
+    // console.log('Service worker capturou um evento do tipo fetch.');
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
+        })
+    );
 
 });
-
