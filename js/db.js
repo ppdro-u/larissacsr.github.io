d2746de284ec45075fb5e478c89e6ee4b3cc49f5
Larissa Reis <larissa.candeia@gmail.com>
2019-10-30 16:42:17 -0300

    mudei o nome da imagem

---
+++ js/db.js
@@ -0,0 +1,14 @@
+// real-time listener que verifica as mudanças que ocorrem
+db.collection('recipes').onSnapshot(snapshot => {
+  //console.log(snapshot.docChanges());
+  snapshot.docChanges().forEach(change => {
+    //console.log(change.type, change.doc.id, change.doc.data());
+    if(change.type === 'added'){
+      // add the document data to the web page
+      renderRecipe(change.doc.data(), change.doc.id);
+    }
+    if(change.type === 'removed'){
+      // remove the document data from the web page
+    }
+  });
+});
