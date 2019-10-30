dcad1faecb4ceabf194ea80324f579fe04a1fb60
Larissa Reis <larissa.candeia@gmail.com>
2019-10-30 16:41:36 -0300

    criei o render recipes no ui.js, deletei as receitas que existiam.

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,5 @@
+const sobremesas = document.querySelector('.sobremesas');
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
@@ -5,3 +7,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // $('#textarea1').val('Novo Texto');
 // M.textareaAutoResize($('#textarea1'));
+
+const renderRecipe = (data, id) => {
+
+    const html = `
+  <div class="col s12 m6 l3 sobremesa" data-id="${id}">
+	    <div class="card" style="background-color: #A59C94FF;">
+		    <div class="card-image sobremesa-imagem">
+	            <img src="${data.endereco_imagem}">
+	            <span class="card-title sobremesa-titulo">"${data.nome}"</span>
+	        </div>
+	        <div class="card-content sobremesa-descricao">
+	            <p>"${data.descricao}"</p>
+	        </div>
+	        <div class="card-action">
+		        <a style="color: #AE0E36FF;" class="sobremesa-link" href="${sobremesa.link}">Saiba mais</a>
+	        </div>
+	        <div class="sobremesa-deletar">
+                <i class="material-icons" data-id="${id}">delete_outline</i>
+            </div>
+	    </div>
+   </div>`;
+    sobremesas.innerHTML += html;
+
+};
